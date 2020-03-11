1944e32c03130e68a3c3b1553992c2a88f72d08d
Mikel Sanchez <mikelsmartinez@aol.com>
2020-03-11 10:23:29 +0000

    added fancy message and shellcheck fix

--- update.sh
+++ update.sh
@@ -7,17 +7,17 @@ update_repository(){
 
 enter_folder_and_print_inside_exit(){
     cd "$1" || exit
-
+    
     # Check folder is git repository
     if [ -d .git ]; then
-        base_name=$(basename $(git rev-parse --show-toplevel))
+        base_name=$(basename "$(git rev-parse --show-toplevel)")
         echo "Updating repository $base_name"
         update_repository
     else
         folder_name=${PWD##*/}
         echo "$folder_name is not a Git Repository"
     fi
-
+    
     cd ..
 }
 
@@ -27,6 +27,25 @@ loop_folders(){
     done
 }
 
+show_message(){
+    
+    echo "
+
+ _    _  ___   ____        _______ ______    _____ _____ _______
+| |  | |  __ \|  __ \   /\|__   __|  ____|  / ____|_   _|__   __|
+| |  | | |__) | |  | | /  \  | |  | |__    | |  __  | |    | |
+| |  | |  ___/| |  | |/ /\ \ | |  |  __|   | | |_ | | |    | |
+| |__| | |    | |__| / ____ \| |  | |____  | |__| |_| |_   | |
+ \____/|_|    |_____/_/    \_\_|  |______|  \_____|_____|  |_|
+                                                     by: sWallyx
+
+    "
+    
+}
+
+clear
+
+show_message
 
 echo "Thanks for using me!"
 
