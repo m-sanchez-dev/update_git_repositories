327e0b0a436acaa9a0746f6c2bcf099ecb9a0acd
Mikel Sanchez <mikelsmartinez@aol.com>
2020-03-19 16:30:12 +0000

    Add messages

--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+printf "Let me install the script for you, it will only be one second\n"
+
+printf "Installation started\n"
+
+printf "I need to make this executable\n"
 # Make script executable
 chmod +x update.sh
 
-# Save path
-
+# Get repository path
 repository_folder=$(pwd)
+printf "The script is being installed in: $repository_folder\n"
 
 # Save alias
-echo "$repository_folder"
-#alias git-update="/home/bob/programming/libs/intellij/bin/idea.sh"
+printf "Setting alias"
+# alias git-update="$repository_folder/update.sh"
+
+printf "Installation completed!\n\n"
+printf "You can use the command by typing git-update in the terminal\n"
