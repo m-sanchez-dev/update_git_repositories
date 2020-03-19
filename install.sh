2743fa4259be0aaf9b87133842434ccf62f70dd7
Mikel Sanchez <mikelsmartinez@aol.com>
2020-03-19 16:19:29 +0000

    Add executable and get route

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Make script executable
+chmod +x update.sh
+
+# Save path
+
+repository_folder=$(pwd)
+
+# Save alias
+echo "$repository_folder"
+#alias git-update="/home/bob/programming/libs/intellij/bin/idea.sh"
