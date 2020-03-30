7ba1f047ee8583ace4965f5255ca8b20a2352f71
sWallyx <mikelsmartinez@aol.com>
2020-03-30 17:30:36 +0100

    updated the current branch command, closes #6

--- functions-git.sh
+++ functions-git.sh
@@ -4,7 +4,7 @@
 update_via_git_master(){
     # Check script is last version of master
     # Get with branch in
-    current_branch=$(git branch --show-current)
+    current_branch=$(git rev-parse --abbrev-ref HEAD)
     if [[ $current_branch != 'master' ]]; then
         # If not in master stash changes move to master
         git stash
